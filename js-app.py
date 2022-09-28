a030ff381974964f87cde5893b16185120ebe05d
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-09-27 21:46:00 -0500

    Delete js-app.py

--- js-app.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Example")
-root.geometry("500x300")
-root.iconbitmap("js-app.ico")
-root.resizable(False, False)
-button = tk.Button(root, text="Close App", command=root.destroy)
-button.place(x=10, y=10, width=100, height=50)
-root.mainloop()
